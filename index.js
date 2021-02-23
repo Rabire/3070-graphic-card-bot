207484b7fb04fefa51632bc04233372414636530
Rabire <rabireh@outlook.fr>
2021-02-23 21:42:48 +0100

    ldlc

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const fetch = require("node-fetch");
 const data = require("./data.json");
+const moment = require("moment");
 
 const articles = data.articles;
 
 let round = 0;
+
+const alert = (url) => {
+  console.log(`${moment().format("LTS")}: CG EN STOCK: ${url}`);
+};
 
 (async () => {
   console.log(`running...`);
@@ -11,14 +16,28 @@ let round = 0;
   while (true) {
     const articleUrl = articles[round % articles.length];
 
+    // console.log(`TESTING ${articleUrl.substring(94)} !`);
+
     await fetch(articleUrl)
       .then(async (res) => {
+        // console.log(`GOT ${articleUrl} !`);
         const reponse = await res.text();
 
-        // console.log(`${articleUrl.substring(94)} DONE !`);
+        if (articleUrl.includes("ldlc")) {
+          const stockDiv = reponse.substring(
+            reponse.lastIndexOf("<aside>") + 1,
+            reponse.lastIndexOf("</aside>")
+          );
 
-        if (!reponse.includes("Rupture de stock")) {
-          console.log(`CG EN STOCK: ${articleUrl}`);
+          if (!stockDiv.includes("Rupture")) {
+            alert(articleUrl);
+          }
+
+          // alert(articleUrl);
+        } else if (articleUrl.includes("topachat")) {
+          if (!reponse.includes("Rupture de stock")) {
+            alert(articleUrl);
+          }
         }
       })
       .catch((e) => console.log(e))
